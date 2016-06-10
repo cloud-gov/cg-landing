69a72180fbbcf1878f6f83cd0242100f8abc01e0
Aidan Feldman <aidan.feldman@gsa.gov>
2016-06-10 02:51:58 -0400

    share build steps between concourse and wercker

--- ci/build.sh
+++ ci/build.sh
@@ -3,21 +3,6 @@
 set -e
 set -x
 
-# install dependencies
-# https://github.com/ffi/ffi/issues/485#issuecomment-209778567
-# https://github.com/gliderlabs/docker-alpine/issues/53#issuecomment-179486583
-apk add --update build-base libffi-dev libxml2-dev libxslt-dev
-bundle config build.nokogiri --use-system-libraries
-bundle
-
-# validate about.yml
-bundle exec about_yml_validate ./.about.yml
-
-# build site
-bundle exec jekyll build --trace
-
-# validate links
-bundle exec htmlproof --alt-ignore "/^\/assets\/img\/i-/" --disable-external ./_site
-
+source $(dirname $0)/build-shared.sh
 # copy files to output directory, so that they can be read by subsequent step
 cp -R . ../cg-landing-compiled
